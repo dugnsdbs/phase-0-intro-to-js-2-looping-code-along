ed6ce9f223ec805fbc07a104ca37175aa1c002d4
Yeohoon <yhoon.yoon@gmail.com>
2021-12-08 23:35:22 -0500

    Completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+let name = ["Guadalupe", "Ollie", "Aki"];
+let word = "surprise";
+
+
+function writeCards (array, string){
+  let newArray =[];
+  let i = 0;
+  while (i < array.length){
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ` + string + " gift!");
+    i++;
+  }
+  return newArray;
+}
+
+function countDown(){
+  for (let i = 10; i >= 0; i--){
+    console.log(i)
+  }
+}
